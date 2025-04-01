dea1c69bbd188168746a835c3bf985bc58367bf4
Leonardo Felipe Ribas Ferreira <Leonardo_felipe1@outlook.com>
2025-04-01 11:36:14 -0300

    Update BancoFacade.java

--- src/main/java/facade/BancoFacade.java
+++ src/main/java/facade/BancoFacade.java
@@ -1,1 +1,21 @@
+package com.dio.project.facade;
+
+import com.dio.project.strategy.Deposito;
+import com.dio.project.strategy.OperacaoBancaria;
+import com.dio.project.strategy.Saque;
+
+public class BancoFacade {
+    public void realizarOperacao(String tipo, double valor) {
+        OperacaoBancaria operacao;
+        if ("deposito".equalsIgnoreCase(tipo)) {
+            operacao = new Deposito();
+        } else if ("saque".equalsIgnoreCase(tipo)) {
+            operacao = new Saque();
+        } else {
+            System.out.println("Operação inválida");
+            return;
+        }
+        operacao.executar(valor);
+    }
+}
 
