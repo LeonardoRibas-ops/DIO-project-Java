f0b025b127e6771ecec01cf3a3e526770717760a
Leonardo Felipe Ribas Ferreira <Leonardo_felipe1@outlook.com>
2025-04-01 11:33:19 -0300

    Create ContaBancaria.java

---
+++ src/main/java/model/ContaBancaria.java
@@ -0,0 +1,17 @@
+package com.dio.project.model;
+
+public class ContaBancaria {
+    private double saldo;
+
+    public ContaBancaria() {
+        this.saldo = 0.0;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+}
