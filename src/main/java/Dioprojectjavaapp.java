a5e0129bc133d29fb382c93b93d051e20bb1a484
Leonardo Felipe Ribas Ferreira <Leonardo_felipe1@outlook.com>
2025-04-01 11:28:22 -0300

    Create Dioprojectjavaapp.java

---
+++ src/main/java/Dioprojectjavaapp.java
@@ -0,0 +1,11 @@
+package com.dio.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioProjectJavaApp {
+    public static void main(String[] args) {
+        SpringApplication.run(DioProjectJavaApp.class, args);
+    }
+}
