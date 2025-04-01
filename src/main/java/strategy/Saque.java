72172cd63c1e5f3d5a73d3cb5f232cc2ec8ae763
Leonardo Felipe Ribas Ferreira <Leonardo_felipe1@outlook.com>
2025-04-01 11:35:55 -0300

    Update Saque.java

--- src/main/java/strategy/Saque.java
+++ src/main/java/strategy/Saque.java
@@ -1,1 +1,17 @@
+package com.dio.project.strategy;
+
+import com.dio.project.singleton.BancoSingleton;
+
+public class Saque implements OperacaoBancaria {
+    @Override
+    public void executar(double valor) {
+        BancoSingleton banco = BancoSingleton.getInstance();
+        if (banco.getSaldo() >= valor) {
+            banco.setSaldo(banco.getSaldo() - valor);
+            System.out.println("Saque de R$" + valor + " realizado. Saldo atual: R$" + banco.getSaldo());
+        } else {
+            System.out.println("Saldo insuficiente para saque de R$" + valor);
+        }
+    }
+}
 
