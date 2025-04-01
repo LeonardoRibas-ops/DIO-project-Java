e7531c540d4a2ef1e27381d6c3f67d8995ffa6aa
Leonardo Felipe Ribas Ferreira <Leonardo_felipe1@outlook.com>
2025-04-01 11:36:40 -0300

    Update BancoController.java

--- src/main/java/controller/BancoController.java
+++ src/main/java/controller/BancoController.java
@@ -1,1 +1,20 @@
+package com.dio.project.controller;
+
+import com.dio.project.service.BancoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/banco")
+public class BancoController {
+    
+    @Autowired
+    private BancoService bancoService;
+
+    @PostMapping("/operacao")
+    public String realizarOperacao(@RequestParam String tipo, @RequestParam double valor) {
+        bancoService.realizarOperacao(tipo, valor);
+        return "Operação de " + tipo + " realizada com sucesso!";
+    }
+}
 
