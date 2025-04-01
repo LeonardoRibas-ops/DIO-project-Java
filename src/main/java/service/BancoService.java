30382963474a7807206ca51f6a4af805946c3f76
Leonardo Felipe Ribas Ferreira <Leonardo_felipe1@outlook.com>
2025-04-01 11:36:28 -0300

    Update BancoService.java

--- src/main/java/service/BancoService.java
+++ src/main/java/service/BancoService.java
@@ -1,1 +1,14 @@
+package com.dio.project.service;
+
+import com.dio.project.facade.BancoFacade;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BancoService {
+    private final BancoFacade bancoFacade = new BancoFacade();
+
+    public void realizarOperacao(String tipo, double valor) {
+        bancoFacade.realizarOperacao(tipo, valor);
+    }
+}
 
