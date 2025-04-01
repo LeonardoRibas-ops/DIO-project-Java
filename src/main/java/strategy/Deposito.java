ff611a8000c564a32f1871afce8ac72da5935e3a
Leonardo Felipe Ribas Ferreira <Leonardo_felipe1@outlook.com>
2025-04-01 11:35:47 -0300

    Update Deposito.java

--- src/main/java/strategy/Deposito.java
+++ src/main/java/strategy/Deposito.java
@@ -1,1 +1,13 @@
+package com.dio.project.strategy;
+
+import com.dio.project.singleton.BancoSingleton;
+
+public class Deposito implements OperacaoBancaria {
+    @Override
+    public void executar(double valor) {
+        BancoSingleton banco = BancoSingleton.getInstance();
+        banco.setSaldo(banco.getSaldo() + valor);
+        System.out.println("Depósito de R$" + valor + " realizado. Saldo atual: R$" + banco.getSaldo());
+    }
+}
 
