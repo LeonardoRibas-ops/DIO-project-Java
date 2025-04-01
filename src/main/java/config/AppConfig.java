ee05bc140787b71dda51548afdd0eba835bdcb4b
Leonardo Felipe Ribas Ferreira <Leonardo_felipe1@outlook.com>
2025-04-01 11:31:08 -0300

    Create AppConfig.java

---
+++ src/main/java/config/AppConfig.java
@@ -0,0 +1,8 @@
+package com.dio.project.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    // Configurações gerais da aplicação
+}
